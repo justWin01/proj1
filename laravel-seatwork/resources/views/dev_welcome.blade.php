961760303c0b9952154e01f5fdeda7f0d0b35dcc
codeUP <labesherwin@gmail.com>
2026-03-07 18:15:48 +0800

    add

---
+++ laravel-seatwork/resources/views/dev_welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Seatwork</title>
+</head>
+
+<body>
+
+    <h1>Laravel Dev: Seatwork #2</h1>
+
+    <p>
+        Current Date: <?php echo date('F d, Y'); ?>
+    </p>
+
+    <footer>
+        Submitted by: Sherwin Labe
+    </footer>
+
+</body>
+</html>
--- laravel-seatwork/routes/web.php
+++ laravel-seatwork/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome-dev', function () {
+    return view('dev_welcome');
 });
