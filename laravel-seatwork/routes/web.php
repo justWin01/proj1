936361e083934731910d97feb78f19f5952bd463
codeUP <labesherwin@gmail.com>
2026-03-09 15:13:20 +0800

    add

--- laravel-seatwork/routes/web.php
+++ laravel-seatwork/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/welcome-dev', function () {
     return view('dev_welcome');
 });
+
+Route::get('/developer/{name}', function ($name) {
+    return view('developer', ['name' => $name]);
+});
