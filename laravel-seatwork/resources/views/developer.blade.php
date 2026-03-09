f55aacf425897d37e140756948a4846a8931f310
codeUP <labesherwin@gmail.com>
2026-03-09 15:17:31 +0800

    changes

---
+++ laravel-seatwork/resources/views/developer.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Developer Page</title>
+</head>
+<body>
+
+    <h1>Laravel Developer Page</h1>
+
+    <p>Welcome, {{ $name }}!</p>
+
+    <p>You are currently exploring Laravel routing and Blade templates.</p>
+
+    <footer>
+    Seatwork #3
+    </footer>
+
+</body>
+</html>
